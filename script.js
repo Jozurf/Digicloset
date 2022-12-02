dc34a200c9e83574dfb67bdb48bd08a4eef0048c
Jozurf <114124627+Jozurf@users.noreply.github.com>
2022-12-02 11:09:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+//Model
+
+
+//View
+const openForm = () => {
+    document.getElementById('clothingform').style.display = "block"
+    console.log('You did it')
+}
+
+const closeForm = () => {
+    document.getElementById('clothingform').style.display = "none"
+}
+
+//Controller
